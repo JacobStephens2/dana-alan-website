7329ccf26068a159ebc88a85d18c97da6d5c4785
Jacob Stephens <53905376+JacobStephens2@users.noreply.github.com>
2021-08-16 17:35:58 -0400

    add donation page and refine links

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -1,6 +1,35 @@
 import React from "react"
 import Header from '../components/header'
+import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
-export default function Donte() {
-  return <Header pageTitle="Donate" />
+
+const Donate = ({ data }) => {
+  return (
+    <div>
+      <Header pageTitle="Donate" />
+      <Container className="top-space">
+        <Row>
+          <Col md={10} lg={7} xl={6}>
+            <MDXRenderer>
+              {data.mdx.body}
+            </MDXRenderer>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  )
 }
+
+export const query = graphql`
+  query {
+    mdx(slug: {eq: "donate"}) {
+      body
+    }
+  }
+`
+
+export default Donate
