af0bd3605469d449255ccdebfe8033f940b3bb9c
Jacob Stephens <53905376+JacobStephens2@users.noreply.github.com>
2021-08-30 12:23:07 -0400

    add attribution to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,26 +12,29 @@ const Footer = () => {
 			<Container>
 
 			<Row>
-				<Col xs={4} sm={3} md={4} lg={0} className="donate-column">
+				<Col xs={4} sm={6} md={2} lg={0} className="donate-column">
 					<div className="donate-button-box">
 						<Link to="/donate" className="nav-link menu-donate donate-button" activeClassName="active">Donate</Link>
 					</div>
 				</Col>	
 
-				<Col xs={8} sm={4} lg={6}>
+				<Col xs={8} sm={6} md={4} lg={6} className="contact-column">
 					<p className="contact">
 					<MailIcon />
 					&ensp;	
 					<a href="mailto:danaalanWVT@gmail.com">danaalanWVT@gmail.com</a>
 					</p>
 				</Col>	
-
-
-				<Col className="copyright-column align-items-center">
-					<Link className="copyright" to="/attributions">
-						<div>&copy;&thinsp;{(new Date().getFullYear())} Dana Alan</div>
-					</Link>
+			
+				<Col xs={12} sm={6} md={4} lg={4} className="attribution-column">
+					<div className="copyright attribution">Designed and Donated&nbsp;by&ensp;<a className="attribution-link" href="https://stewardgoods.com">15&nbsp;East&nbsp;Creative</a></div>
 				</Col>	
+
+				<Col xs={12} sm={6} md={2} className="copyright-column align-items-center">
+					<div className="copyright"><Link to="/attributions">&copy;&thinsp;{(new Date().getFullYear())} Dana&nbsp;Alan</Link>
+					</div>
+				</Col>	
+
 			</Row>
 
 
